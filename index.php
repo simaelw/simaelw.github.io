e48dea19bdcf322a4b08a3d1bb8ed16e4231d90b
simaelw <simcot89@gmail.com>
2024-08-06 13:52:42 +0200

    updated index

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello World</title>
+    <title>Za Wardo</title>
 </head>
 
 <body>
-    <h1>Hello World</h1>
+    <h1>Za Wardo</h1>
 </body>
 
 </html>
